c70b9f97b4448fc1b4e99e46b48ab228934a72d9
GBTC441017UR <fadhld92@gmail.com>
2018-06-08 12:41:32 -0400

    "This wont take long version"

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,23 +1,53 @@
 
+import java.util.*;
+
 
 public class FizzBuzz {
+    static Scanner scan = new Scanner(System.in);
+    static Random r = new Random();
+    static int startingNum, endingNum;
+
+
 
     public static void main(String[] args) {
+        FizzBuzz();
 
-        for (int i = 1; i <= 100; i++)
-            System.out.println(FizzBuzz(i));
     }
 
-    public static String FizzBuzz(int i) {
-        if (i % 3 == 0 && i % 5 == 0)
-            return "Fizz Buzz";
-        else if (i % 5 == 0)
-            return "Buzz";
-        else if (i % 3 == 0)
-            return "Fizz";
-        else if (i % 7 == 0)
-            return String.valueOf(i) + " - multiple of seven";
-        else
-            return String.valueOf(i);
+    public static void FizzBuzz() {
+
+        startingNum = 1 + r.nextInt(100);
+        endingNum = 1 + r.nextInt(100);
+
+
+        int value = endingNum - startingNum;
+
+        if (startingNum == endingNum) {
+            System.out.println(startingNum);
+            System.out.println(endingNum);
+            System.out.println("Shazam!");
+
+            // if start & last number are last tha 10 print won't take long
+        } else if (value < 10) {
+            System.out.println("This won't take long!");
+        }
+
+        for (int i = startingNum; i < endingNum; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }else if (i % 7 == 0){
+                System.out.println(i + " -multiple of seven ");
+
+            } else {
+                System.out.println(i);
+
+
+            }
+        }
     }
 }
